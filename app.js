3f0a0c96a822acb8e88d5466b043dd70ae4e3852
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-06-22 06:11:45 -0400

    commented the answers from class

--- app.js
+++ app.js
@@ -21,17 +21,17 @@ for (let x = 1; x <=100; x++) {     //x is equal to one; it's less then or equal
 }
 */
 
-/*
+
 //even using while loop
 let z = 1;
 
 while (z <=100) {
-    if (z % 2 ==0) {                   //print out the number if there is not a remainder
+    if (z % 2 ==0) {                //print out the number if there is not a remainder
         console.log (z); 
     }
     z++;
 }
-*/
+
 
 /*
 for (let zz = 0; zz <=100; zz +=2) {
@@ -40,7 +40,7 @@ for (let zz = 0; zz <=100; zz +=2) {
 
 */
 
-/*
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
@@ -58,12 +58,13 @@ for (let a = 1; a <=100; a++) {
     if (a % 3 && a % 5 ==0)
     console.log ("FIZZBUZZ")
 }
-*/
 
-//or
 
+//or this was given in class
+/*
 for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) { console.log ('${i} FIZZBUZZ');}
     else if (i % 3 == 0) { console.log ('${i} FIZZ');} 
     else if (i % 5 == 0) { console.log ('${i} BUZZ');}
 }
+*/
